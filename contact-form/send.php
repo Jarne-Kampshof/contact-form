45c1602837a5c323c62bc0abea05b498df82c6d2
Jarne <jarne.kampshof@student.graafschapcollege.nl>
2026-03-24 08:16:04 +0000

    d

---
+++ contact-form/send.php
@@ -0,0 +1,24 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$to = "jouwnaam@student.gc-webhosting.nl";
+
+// Email settings
+$subject = "New Contact Form Submission";
+$body = "Name: $name\nEmail: $email\nMessage: $message";
+
+$headers = "From: noreply@student.gc-webhosting.nl";
+
+$headers += "\r\nReply-To: $email";
+
+// Send email
+if (mail($to, $subject, $body, $headers)) {
+    echo "Email sent successfully!";
+} else {
+    echo "Failed to send email.";
+}
+
+?>
